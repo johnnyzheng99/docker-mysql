8ce43c4bcd4be7fa29e117f16c965a6fac969e4a
johnny <johnny@itfolks.com.au>
2015-01-21 03:29:00 +1100

    Initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,15 @@
-FROM ubuntu:14.04
-MAINTAINER MarvAmBass
+FROM ubuntu:trusty
+MAINTAINER johnny@itfolks.com.au
 
-RUN apt-get update && apt-get install -y \
-    mysql-server \
-    mysql-client
-
-RUN rm -rf /var/lib/mysql/*
+RUN apt-get install -y mysql-client
 
 ADD mysql-backuper.sh /usr/local/bin/mysql-backuper.sh
 RUN chmod a+x /usr/local/bin/mysql-backuper.sh
 
-ENV MYSQL_DEFAULTS_FILE /mysql-defaults.cnf
-
-EXPOSE 3306
-
 ADD ./startup.sh /opt/startup.sh
 RUN chmod a+x /opt/startup.sh
 CMD ["/bin/bash", "/opt/startup.sh"]
+
+ENV DB_ENV_MYSQL_USER admin
+ENV BACKUP_CRON_TIME 0 * * * *
+ENV BACKUP_ENABLED yes
--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+: ${DB_ENV_MYSQL_HOST:="db"}; : ${DB_ENV_MYSQL_USER:="admin"}; : ${DB_ENV_MYSQL_PASS:=""}; : ${MYSQL_DEFAULTS_FILE:="/mysql.cnf"}; : ${BACKUP_CRON_TIME:="0 * * * *"}; : ${BACKUP_ENABLED:="yes"};
+
 ## FUNCTIONS
 
 function exit_if_no_credentials_provided {
@@ -8,18 +10,6 @@ function exit_if_no_credentials_provided {
     >&2 echo ">> you need credentials for this action but no credentials were provided!";
     exit 1
   fi
-}
-
-function init_db {
-  mysql_install_db
-
-  /usr/bin/mysqld_safe &
-  sleep 10s
-
-  echo "GRANT ALL ON *.* TO $ADMIN_USER@'%' IDENTIFIED BY '$ADMIN_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES" | mysql
-
-  killall mysqld
-  sleep 10s
 }
 
 function enable_backups {
@@ -42,8 +32,11 @@ function enable_backups {
 function create_mysql_defaults_file {
 cat <<EOF > "$MYSQL_DEFAULTS_FILE"
 [client]
-user="$ADMIN_USER"
-password="$ADMIN_PASSWORD"
+user="$DB_ENV_MYSQL_USER"
+password="$DB_ENV_MYSQL_PASS"
+host="$DB_ENV_MYSQL_HOST"
+[mysqldump]
+host="$DB_ENV_MYSQL_HOST"
 EOF
 }
 
@@ -54,46 +47,18 @@ EOF
 MY_IP=`ip a s eth0 | grep inet | awk '{print $2}' | sed 's/\/.*//g' | head -n1`
 
 CREDENTIALS_PROVIDED="yes"
-if [ -z ${ADMIN_USER+x} ]
+if [ -z ${DB_ENV_MYSQL_PASS+x} ]
 then
-  >&2 echo ">> no \$ADMIN_USER specified" 
-  ADMIN_USER="\$ADMIN_USER"
-  CREDENTIALS_PROVIDED="no"
-fi
-if [ -z ${ADMIN_PASSWORD+x} ]
-then
-  >&2 echo ">> no \$ADMIN_PASSWORD specified"
+  >&2 echo ">> no \$DB_ENV_MYSQL_PASS specified"
   CREDENTIALS_PROVIDED="no"
 fi
 
 # backup stuff
-if [ "yes" != "$BACKUP_ENABLED" ]
-then
-  echo ">> disable auto-backups (mysqldump)"
-else
-  echo ">> enable auto-backups (mysqldump)"
-  echo ">> backups will be stored at default path"
-  echo ">> !! link or overwrite it to gain access !!"
-  exit_if_no_credentials_provided
-  enable_backups
-fi
+echo ">> enable auto-backups (mysqldump)"
+echo ">> backups will be stored at default path"
+echo ">> !! link or overwrite it to gain access !!"
+exit_if_no_credentials_provided
+enable_backups
 
-# mysql daemon stuff
-if [ -z ${BIND_ADDRESS+x} ]
-then
-  BIND_ADDRESS="0.0.0.0"
-fi
-echo ">> bind mysql daemon to $BIND_ADDRESS"
-sed -i -e "s/^bind-address\s*=\s*127.0.0.1/bind-address = $BIND_ADDRESS/" /etc/mysql/my.cnf
-
-if [ ! -f /var/lib/mysql/ibdata1 ]; then
-  echo ">> init db"
-  exit_if_no_credentials_provided
-  init_db
-fi
-echo ">> db installed"
-
-echo ">> starting mysql daemon"
-echo ">> you can connect via mysql cli with the following command:"
-echo "   mysql -u $ADMIN_USER -p -h $MY_IP"
-/usr/bin/mysqld_safe
+echo ">> starting daemon"
+/bin/bash -c "while true;do sleep ${SLEEP_SECONDS:-30};done"
